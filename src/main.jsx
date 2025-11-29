f2ae8e12ba7a3d6936afb33e25a198e703e3b092
CJayRomero <christianjayromero0101@gmail.com>
2025-11-29 12:44:59 +0800

    Implement Home component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Todos from "./pages/Todos";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/todos" element={<Todos />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<App />);
